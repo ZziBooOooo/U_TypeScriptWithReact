a6b5e60c0106931f897d4fe57d8c99214fe75c10
minsuh Jo <zziboo3773@gmail.com>
2025-04-02 17:56:03 +0900

    FEAT : Container 컴포넌트 추가

--- New Project/src/App.tsx
+++ New Project/src/App.tsx
@@ -1,17 +1,19 @@
 import Input from "./components/Input";
 import Button from "./components/Button";
+import Container from "./components/Container";
 
 function App() {
   return (
     <main>
       {/* <Input id="name" label="Your name" type="text"></Input>
       <Input id="age" label="Your age" type="number"></Input> */}
-      <p>
+      {/*       <p>
         <Button>A Button</Button>
       </p>
       <p>
         <Button href="https://www.google.co.kr/?hl=ko">A Link</Button>
-      </p>
+      </p> */}
+      <Container as={Button}>Click me</Container>
     </main>
   );
 }

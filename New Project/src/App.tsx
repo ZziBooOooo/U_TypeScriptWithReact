09c2f1fbe2aa1586e1b0e7c2f78a488432c64abf
ZziBooLang <zziboo3773@gmail.com>
2025-04-03 01:15:02 +0900

    FEAT : Form 컴포넌트 - useImperativeHandle clear메서드

--- New Project/src/App.tsx
+++ New Project/src/App.tsx
@@ -1,29 +1,22 @@
 import Input from "./components/Input";
 import Button from "./components/Button";
 import Container from "./components/Container";
-import Form from "./components/Form";
+import Form, { type FormHandle } from "./components/Form";
 import { useRef, useEffect } from "react";
 
 function App() {
   // const inputRef = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
 
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear();
   }
 
   return (
     <main>
-      {/*       <Input id="name" label="Your name" type="text" ref={inputRef}></Input>
-      <Input id="age" label="Your age" type="number"></Input> */}
-      {/*       <p>
-        <Button>A Button</Button>
-      </p>
-      <p>
-        <Button href="https://www.google.co.kr/?hl=ko">A Link</Button>
-      </p> */}
-      {/* <Container as={Button}>Click me</Container> */}
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name"></Input>
         <Input type="number" label="Age" id="age"></Input>
         <p>
--- New Project/src/components/Form.tsx
+++ New Project/src/components/Form.tsx
@@ -1,23 +1,49 @@
-import { type FormEvent, ComponentPropsWithoutRef } from "react";
+import {
+  type FormEvent,
+  ComponentPropsWithoutRef,
+  useImperativeHandle,
+  useRef,
+  forwardRef,
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
 
-const Form = ({ onSave, children, ...otherProps }: FormProps) => {
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+// forwardRef의 제네릭 첫번째 자리에는
+// ref가 참조할 객체가 clear 메서드를 가진 객체임을 알려줌
+// 즉, ref는 clear메서드를 가진 객체를 참조하고 있다는 것
+const Form = forwardRef<FormHandle, FormProps>(
+  ({ onSave, children, ...otherProps }, ref) => {
+    const form = useRef<HTMLFormElement>(null);
 
-    const formData = new FormData(event.currentTarget);
-    const data = Object.fromEntries(formData);
-    onSave(data);
+    useImperativeHandle(ref, () => {
+      return {
+        clear() {
+          console.log("CLEARING!!");
+          form.current?.reset();
+        },
+      };
+    });
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+      const data = Object.fromEntries(formData);
+      onSave(data);
+    }
+
+    return (
+      <form onSubmit={handleSubmit} {...otherProps} ref={form}>
+        {children}
+      </form>
+    );
   }
-
-  return (
-    <form onSubmit={handleSubmit} {...otherProps}>
-      {children}
-    </form>
-  );
-};
+);
 
 export default Form;

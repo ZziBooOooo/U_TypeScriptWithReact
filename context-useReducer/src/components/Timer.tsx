6fe33be402096b224321a104ce1dba619d2cf27e
minsuh Jo <zziboo3773@gmail.com>
2025-04-04 16:36:27 +0900

    FEAT : useEffect in Timer.tsx

--- context-useReducer/src/components/Timer.tsx
+++ context-useReducer/src/components/Timer.tsx
@@ -1,11 +1,25 @@
 import Container from "./UI/Container.tsx";
 import { Timer as TimerProps } from "../store/timers-context.tsx";
+import { useEffect, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemaningTime] = useState(duration * 1000);
+
+  useEffect(() => {
+    setInterval(function () {
+      setRemaningTime((prevTime) => prevTime - 50);
+    }, 50);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{}</p>
     </Container>
   );
 }

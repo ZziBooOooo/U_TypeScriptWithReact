8c18bf37dd08a5a0263ff54d3d3fa885f4ac42aa
ZziBooLang <zziboo3773@gmail.com>
2025-04-08 21:04:47 +0900

    FEAT : Fetch Blog Data

--- Blog-dataFetching/src/App.tsx
+++ Blog-dataFetching/src/App.tsx
@@ -1,5 +1,49 @@
-function App() {
-  return <h1>Data Fetching!</h1>;
-}
+import { type ReactNode, useEffect, useState } from "react";
 
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+import fetchingImg from "./assets/data-fetching.png";
+
+type RawDataBlogPosts = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPosts[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="An abstract image" />
+      {content}
+    </main>
+  );
+}
 export default App;

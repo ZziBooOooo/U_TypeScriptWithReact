766753b73d2141e1dbc503de7c08a86912ccadd3
ZziBooLang <zziboo3773@gmail.com>
2025-04-10 21:55:51 +0900

    FEAT : useSelector 커스텀 훅 + 타입지정

--- Shop-redux/src/store/hooks.ts
+++ Shop-redux/src/store/hooks.ts
@@ -1,6 +1,15 @@
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "./store";
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from "react-redux";
+import { AppDispatch, RootState } from "./store";
 
 type DispatchFn = () => AppDispatch;
 
 export const useCartDispatch: DispatchFn = useDispatch;
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+/* 
+To be clear : You don't need to re-create these hooks for each of your store slices. You only create them once- hence you might also want to call them useAppDispatch and useAppSelector
+*/
